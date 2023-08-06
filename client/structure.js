7661cfa4ab81fb1496fe3d8775a877f15d3620be
Tom Dobrow <tomdobrow@gmail.com>
2023-08-06 16:23:57 -0500

    client file

---
+++ client/structure.js
@@ -0,0 +1,9 @@
+class Structure {
+  static statsMapping = {
+    Barracks:    { hp: 3,  cost: 50 },
+    Watch_tower: { hp: 3,  cost: 30 },
+    Wall:        { hp: 10, cost: 30 },
+  }
+}
+
+export default Structure;
